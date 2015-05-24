5ce795cf981923c8065b56522c9ee06659087d80
dillonwastrack <dillonwastrack@10.0.0.4>
2015-05-24 02:40:51 -0500

    more cleanup and adding comments

--- Bricks/src/Game.java
+++ Bricks/src/Game.java
@@ -6,8 +6,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
