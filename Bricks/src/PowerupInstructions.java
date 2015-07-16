24cdd8ea124d849f35d2627d7d54644238a5fe25
dillonwastrack <dillonwastrack@10.0.0.3>
2015-07-15 19:38:38 -0500

    added the images.

--- Bricks/src/PowerupInstructions.java
+++ Bricks/src/PowerupInstructions.java
@@ -4,10 +4,13 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import java.awt.Color;
 import javax.swing.UIManager;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 
 public class PowerupInstructions extends JPanel {
-	private final JLabel lblGreen = new JLabel("green ");
+	private final JLabel lblGreen = new JLabel();
+	
 
 	/**
 	 * Create the panel.
@@ -16,6 +19,7 @@ public class PowerupInstructions extends JPanel {
 		setBackground(UIManager.getColor("Button.background"));
 		setLayout(null);
 		lblGreen.setBounds(99, 45, 61, 16);
+		lblGreen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/green powerup.png")));
 		add(lblGreen);
 		
 		
@@ -25,20 +29,37 @@ public class PowerupInstructions extends JPanel {
 		add(lblPowerupInformation);
 		
 		JTextArea txtrGreenColoredPowerups = new JTextArea();
+		txtrGreenColoredPowerups.setEditable(false);
 		txtrGreenColoredPowerups.setBackground(UIManager.getColor("Button.background"));
 		txtrGreenColoredPowerups.setText("Green colored powerups\nare beneficial.");
 		txtrGreenColoredPowerups.setBounds(240, 45, 164, 68);
 		add(txtrGreenColoredPowerups);
 		
-		JLabel lblNewLabel = new JLabel("White");
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/white powerup.png")));
 		lblNewLabel.setBounds(99, 157, 61, 16);
 		add(lblNewLabel);
 		
 		JTextArea txtrWhitePowerupsAre = new JTextArea();
+		txtrWhitePowerupsAre.setEditable(false);
 		txtrWhitePowerupsAre.setBackground(UIManager.getColor("Button.background"));
 		txtrWhitePowerupsAre.setText("White colored powerups \nare neutral.");
 		txtrWhitePowerupsAre.setBounds(240, 157, 164, 50);
 		add(txtrWhitePowerupsAre);
+		
+		JLabel lblI = new JLabel();
+		lblI.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/skull_and_bones2.png")));
+		lblI.setBounds(99, 264, 41, 25);
+		add(lblI);
+		
+		JTextArea txtrYouBetterLeave = new JTextArea();
+		txtrYouBetterLeave.setEditable(false);
+		txtrYouBetterLeave.setBackground(UIManager.getColor("Button.background"));
+		txtrYouBetterLeave.setText("YOU BETTER LEAVE\nTHIS ONE ALONE BOY.\nIF YA KNOW WHATS \nGOOD FOR YA.");
+		txtrYouBetterLeave.setBounds(240, 264, 164, 75);
+		add(txtrYouBetterLeave);
+		
+
 
 	}
 }
